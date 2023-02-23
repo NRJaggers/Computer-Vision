7199940182f85da1af8f142f1317083c2cd3df68
Nathan Jaggers <46210291+NRJaggers@users.noreply.github.com>
2023-02-23 10:53:17 -0800

    Project 5 init

---
+++ Project 5/Project_5_Part_A.m
@@ -0,0 +1,19 @@
+%% Project 5
+%
+% EE/CPE 428 - Computer Vision
+% Winter 2023
+%
+% Group 12: Nathan Jaggers, Fadi Alzammar, Ryan Geisen
+%
+% Description: See coresponding document <Can add description later>
+%% Part A
+close all;
+clear;
+clc;
+
+%%
+
+% use imresize to resize images
+
+% use ROIpoly to crop out areas of interest in image
+
